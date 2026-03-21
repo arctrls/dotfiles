563f29d93e2d6dcff8181398c547414de90de872
Kiwon Lee <jake@ktown4u.com>
2026-03-21 12:33:20 +0900

    update codex alias

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -41,7 +41,7 @@ alias vim='nvim'
 alias ll='eza --long --git --all'
 alias gitr='git reset --hard HEAD && git clean -fd'
 alias cl='claude --dangerously-skip-permissions'
-alias cx='omx --xhigh --madmax'
+alias cx='codex --high --madmax'
 alias cat='bat --paging=never'
 
 if status is-interactive
